427efd3714501255535c6e247e3a6bf1bad17dfb
Dendi Handian <handian.dendi@gmail.com>
2021-11-07 22:09:40 +0700

    header navigation

--- common/components/AppHeader.js
+++ common/components/AppHeader.js
@@ -5,7 +5,17 @@ export default function AppHeader() {
     return (
         <div id="header">
             <div className="px-16 py-8 flex justify-center sm:justify-start">
-                <SpaceXLogo className="text-xs"/>
+                <div className="">
+                    <SpaceXLogo className="text-xs"/>
+                </div>
+                <ul className="list-none flex px-2">
+                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Falcon 9</li>
+                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Falcon Heavy</li>
+                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Dragon</li>
+                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Starship</li>
+                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Human Spaceflight</li>
+                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Rideshare</li>
+                </ul>
             </div>
         </div>
     )
