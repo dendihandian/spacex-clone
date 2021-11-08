f427289f24d9103f678f53d7057e3b6d84e260d9
Dendi Handian <handian.dendi@gmail.com>
2021-11-08 10:10:49 +0700

    sort class using headwind

--- common/components/AppHeader.js
+++ common/components/AppHeader.js
@@ -4,17 +4,20 @@ import SpaceXLogo from './SpaceXLogo'
 export default function AppHeader() {
     return (
         <div id="header">
-            <div className="px-16 py-8 flex justify-center sm:justify-start">
+            <div className="flex justify-center px-16 py-8 sm:justify-start">
                 <div className="">
                     <SpaceXLogo className="text-xs"/>
                 </div>
-                <ul className="list-none flex px-2">
-                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Falcon 9</li>
-                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Falcon Heavy</li>
-                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Dragon</li>
-                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Starship</li>
-                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Human Spaceflight</li>
-                    <li className="mx-2 mt-1 uppercase font-bold tracking-tighter">Rideshare</li>
+                <ul className="hidden pt-2 ml-8 space-x-4 list-none sm:flex">
+                    <li className="text-sm font-bold tracking-tighter uppercase"><a href="#">Falcon 9</a></li>
+                    <li className="text-sm font-bold tracking-tighter uppercase"><a href="#">Falcon Heavy</a></li>
+                    <li className="text-sm font-bold tracking-tighter uppercase"><a href="#">Dragon</a></li>
+                    <li className="text-sm font-bold tracking-tighter uppercase"><a href="#">Starship</a></li>
+                    <li className="text-sm font-bold tracking-tighter uppercase"><a href="#">Human Spaceflight</a></li>
+                    <li className="text-sm font-bold tracking-tighter uppercase"><a href="#">Rideshare</a></li>
+                </ul>
+                <ul className="hidden pt-2 list-none sm:flex">
+                    <li className="text-sm font-bold tracking-tighter uppercase"><a href="#">Shop</a></li>
                 </ul>
             </div>
         </div>
